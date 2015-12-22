99d0ebbac4860c4d2a7f8bf5196ce88d8a9d5c79
rahul <rahul.nagar@infobeans.com>
2015-12-22 19:37:36 +0530

    added new files

--- src/PdfCreator/Generate.php
+++ src/PdfCreator/Generate.php
@@ -6,6 +6,13 @@ class Generate
 {
     public static function PDF()
     {
-        return 'Hello World, Composer!';
+        return
+            '<ol>
+                <li>Simple PDF with default Header and Footer: [<a href="'.$_SERVER['HTTP_REFERER'].'src/HelloWorld/example_001.php" title="PDF [new window]" target="_blank">PDF</a>]</li>
+                <li>Simple PDF without Header and Footer: [<a href="'.$_SERVER['HTTP_REFERER'].'src/HelloWorld/example_002.php" title="PDF [new window]" target="_blank">PDF</a>]</li>
+                <li>Custom Header and Footer: [<a href="'.$_SERVER['HTTP_REFERER'].'src/HelloWorld/example_003.php" title="PDF [new window]" target="_blank">PDF</a>]</li>
+                <li>Cell stretching: [<a href="'.$_SERVER['HTTP_REFERER'].'src/HelloWorld/example_004.php" title="PDF [new window]" target="_blank">PDF</a>]</li>
+                <li>Multicell: [<a href="'.$_SERVER['HTTP_REFERER'].'src/HelloWorld/example_005.php" title="PDF [new window]" target="_blank">PDF</a>]</li>
+            </ol>';
     }
 }
